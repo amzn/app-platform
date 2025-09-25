c4ea97abd453b215fe759061b703353094f90731
Jessalyn Wang <jesslwan@amazon.com>
2025-09-25 15:08:31 -0700

    Remove child views on the main thread and add cross slide viewbinding animations to recipe app

--- recipes/common/impl/src/androidMain/kotlin/software/amazon/app/platform/recipes/backstack/AndroidCrossSlideBackstackRenderer.kt
+++ recipes/common/impl/src/androidMain/kotlin/software/amazon/app/platform/recipes/backstack/AndroidCrossSlideBackstackRenderer.kt
@@ -3,6 +3,8 @@ package software.amazon.app.platform.recipes.backstack
 import android.app.Activity
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.view.animation.Animation
 import me.tatarka.inject.annotations.Inject
 import software.amazon.app.platform.inject.ContributesRenderer
 import software.amazon.app.platform.recipes.common.impl.databinding.CrossSlideViewBinding
@@ -10,6 +12,7 @@ import software.amazon.app.platform.renderer.Renderer
 import software.amazon.app.platform.renderer.RendererFactory
 import software.amazon.app.platform.renderer.ViewBindingRenderer
 import software.amazon.app.platform.renderer.getRenderer
+import software.amazon.app.platform.recipes.common.impl.R
 
 @Inject
 @ContributesRenderer
@@ -24,21 +27,45 @@ class AndroidCrossSlideBackstackRenderer(
   ): CrossSlideViewBinding = CrossSlideViewBinding.inflate(layoutInflater, parent, false)
 
   private var lastRenderer: Renderer<*>? = null
+  private var lastAnimationContentKey: Int? = null
 
   override fun renderModel(model: CrossSlideBackstackPresenter.Model) {
     val modelToRender = model.delegate
     val renderer = rendererFactory.getRenderer(
       modelToRender::class,
       binding.childContainer,
-      modelToRender.hashCode(),
+      model.hashCode(),
     )
 
-    // commenting in the below code ensures the feature does not crash
-//    if (renderer !== lastRenderer) {
-//      binding.childContainer.removeAllViews()
-//      lastRenderer = renderer
-//    }
+    val animationContentKey = model::class.hashCode() + model.backstackScope.lastBackstackChange.value.backstack.size
+    val isPop = model.backstackScope.lastBackstackChange.value.action == PresenterBackstackScope.BackstackChange.Action.POP
 
-    renderer.render(modelToRender)
+    if (animationContentKey != lastAnimationContentKey) {
+      val oldView = binding.childContainer.getChildAt(binding.childContainer.childCount - 1)
+
+      val slideOutAnim = if (isPop) {
+        AnimationUtils.loadAnimation(activity, R.anim.slide_out_right)
+      } else {
+        AnimationUtils.loadAnimation(activity, R.anim.slide_out_left)
+      }
+
+      val slideInAnim = if (isPop) {
+        AnimationUtils.loadAnimation(activity, R.anim.slide_in_left)
+      } else {
+        AnimationUtils.loadAnimation(activity, R.anim.slide_in_right)
+      }
+
+      renderer.render(modelToRender)
+      val newView = binding.childContainer.getChildAt(binding.childContainer.childCount - 1)
+
+      oldView?.startAnimation(slideOutAnim)
+      newView?.startAnimation(slideInAnim)
+    } else {
+      renderer.render(modelToRender)
+    }
+
+    lastAnimationContentKey = animationContentKey
+    lastRenderer = renderer
+
   }
 }
